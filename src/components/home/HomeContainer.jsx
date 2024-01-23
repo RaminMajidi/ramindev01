8364848c79d51bab15a0825e0686ab5ac30016b8
RaminMajidi <raminmajididev01@gmail.com>
2024-01-23 21:18:12 +0330

    minor change

--- src/components/home/HomeContainer.jsx
+++ src/components/home/HomeContainer.jsx
@@ -3,7 +3,7 @@ const HomeContainer = ({children }) => {
     return (
         <section
             className={`w-full h-full flex flex-col items-center px-8
-              md:justify-center md:flex-row-reverse `}
+              md:justify-center md:px-20 md:flex-row-reverse `}
         >
             {children}
         </section>

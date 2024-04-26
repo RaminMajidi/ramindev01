af5eb809552343c458d2ed01b6690a6bcc0fd91a
RaminMajidi <raminmajididev01@gmail.com>
2024-04-26 15:08:01 +0330

    created Not Found Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='flex flex-col justify-center items-center  h-[100dvh]'>
+      <p className='text-4xl text-red-500'>ERROR 404</p>
+      <p className='text-2xl mt-4'>Page Not Found</p>
+    </div>
   )
 }
 
